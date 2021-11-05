dbf8e1d4dbe5469d7898acb4b5113e9103c24d97
Ericson <ericson.gomes.26@gmail.com>
2021-11-05 20:25:40 -0300

    conclusão desafio  aula02

---
+++ src/main/java/fit/core/IrpfCalculator.java
@@ -0,0 +1,35 @@
+package fit.core;
+
+public class IrpfCalculator {
+    public static Double calculateBaseSalary(double totalSalary) {
+        return totalSalary -  330.0;
+    }
+
+    public static Double calculateExemption() {
+        return 1903.98;
+    }
+
+    public static Double calculateDiscount(double baseSalary) {
+        return baseSalary - calculateExemption();
+    }
+
+    public static Double calculateTaxLayer(double baseSalary) {
+        var result  = 0.0;
+        if(baseSalary >= 1903.99 &&  baseSalary <= 2826.65){
+            result = 7.5;
+        }else if(baseSalary >= 2826.66 &&  baseSalary <= 3751.05){
+            result = 15.0;
+        }else if(baseSalary >= 3751.06 &&  baseSalary <= 4664.68){
+            result = 22.5;
+        }else if(baseSalary > 4664.68){
+            result = 27.5;
+        }
+        return result/100;
+    }
+
+    public static Double calculateIrpf(double totalSalary) {
+        var baseSalary = totalSalary * (100-11)/100;
+        return calculateDiscount(baseSalary) * calculateTaxLayer(baseSalary);
+    }
+
+}
