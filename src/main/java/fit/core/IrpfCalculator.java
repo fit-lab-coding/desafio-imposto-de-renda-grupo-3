0691128d0c289c7f66c2dafd9a90f4269235da8d
Ericson <ericson.gomes.26@gmail.com>
2021-11-05 20:32:45 -0300

    refactory calculateBaseSalary

--- src/main/java/fit/core/IrpfCalculator.java
+++ src/main/java/fit/core/IrpfCalculator.java
@@ -2,7 +2,7 @@ package fit.core;
 
 public class IrpfCalculator {
     public static Double calculateBaseSalary(double totalSalary) {
-        return totalSalary -  330.0;
+        return totalSalary *  (100-11)/100;
     }
 
     public static Double calculateExemption() {
@@ -15,11 +15,11 @@ public class IrpfCalculator {
 
     public static Double calculateTaxLayer(double baseSalary) {
         var result  = 0.0;
-        if(baseSalary >= 1903.99 &&  baseSalary <= 2826.65){
+        if(baseSalary >= 1903.99 && baseSalary <= 2826.65){
             result = 7.5;
-        }else if(baseSalary >= 2826.66 &&  baseSalary <= 3751.05){
+        }else if(baseSalary >= 2826.66 && baseSalary <= 3751.05){
             result = 15.0;
-        }else if(baseSalary >= 3751.06 &&  baseSalary <= 4664.68){
+        }else if(baseSalary >= 3751.06 && baseSalary <= 4664.68){
             result = 22.5;
         }else if(baseSalary > 4664.68){
             result = 27.5;
@@ -28,7 +28,7 @@ public class IrpfCalculator {
     }
 
     public static Double calculateIrpf(double totalSalary) {
-        var baseSalary = totalSalary * (100-11)/100;
+        var baseSalary = calculateBaseSalary(totalSalary);
         return calculateDiscount(baseSalary) * calculateTaxLayer(baseSalary);
     }
 
